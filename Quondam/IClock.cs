20728a6a678a86bdafb4565fcad4d28390d32782
Martin Eden <martin.s.eden@gmail.com>
2016-11-28 11:18:27 +0000

    Add some commentary

--- Quondam/IClock.cs
+++ Quondam/IClock.cs
@@ -2,6 +2,17 @@
 
 namespace MartinEden.Quondam
 {
+    /// <summary>
+    /// A clock provides a way of getting the current time. The purpose here is to take the
+    /// global static DateTime object, and wrap it in an interface that allows us to mock
+    /// it in the tests. That way, the tests can control timing precisely.
+    /// 
+    /// Note that this in an internal interface, and the PasswordManager constructor that
+    /// takes a clock is also internal. This is so that this detail is hidden from consumers
+    /// of the library - they just get the expected behaviour with a real clock. Because the
+    /// unit tests have access to internal members of this library, they can still make use
+    /// of this interface and the constructor.
+    /// </summary>
     internal interface IClock
     {
         DateTime Now { get; }
--- Quondam/IPasswordManager.cs
+++ Quondam/IPasswordManager.cs
@@ -2,7 +2,16 @@ namespace MartinEden.Quondam
 {
     public interface IPasswordManager
     {
+        /// <summary>
+        /// Generate a 16 character password for any given username. This password
+        /// is guaranteed to be distinct from any other user's password.
+        /// </summary>
         string GenerateOneTimePassword(string username);
+        /// <summary>
+        /// Returns true if the provided password was the one generated for the user
+        /// the last time GenerateOneTimePassword was called for that user. Only
+        /// returns true if the password was generated within the last 30 seconds.
+        /// </summary>
         bool ValidatePassword(string username, string password);        
     }
 }
